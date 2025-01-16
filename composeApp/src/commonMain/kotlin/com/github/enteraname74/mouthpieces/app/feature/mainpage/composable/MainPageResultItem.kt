fa57bc9a9c57d168b4ad059ab619f937325a1bc8
TommyDIL <147648136+TommyDIL@users.noreply.github.com>
2025-01-16 14:42:06 +0100

    Improve item list view

---
+++ composeApp/src/commonMain/kotlin/com/github/enteraname74/mouthpieces/app/feature/mainpage/composable/MainPageResultItem.kt
@@ -0,0 +1,35 @@
+package com.github.enteraname74.mouthpieces.app.feature.mainpage.composable
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.github.enteraname74.mouthpieces.domain.model.Mouthpiece
+
+@Composable
+fun MainPageResultItem(mouthpiece: Mouthpiece) {
+    ElevatedCard(Modifier.width(250.dp)) {
+        Box(Modifier.fillMaxSize().padding(12.dp)) {
+            Column {
+                Text(
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.headlineSmall,
+                    text = mouthpiece.name,
+                )
+
+                Spacer(Modifier.height(12.dp))
+
+                Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+                    Text("Saxophone : " + mouthpiece.saxophone.value)
+                    Text("Genre : " + mouthpiece.genre.value)
+                    Text("Chamber : " + mouthpiece.chamber)
+                    Text("Baffle : " + mouthpiece.baffle)
+                }
+            }
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/com/github/enteraname74/mouthpieces/app/feature/mainpage/composable/MainPageResultList.kt
+++ composeApp/src/commonMain/kotlin/com/github/enteraname74/mouthpieces/app/feature/mainpage/composable/MainPageResultList.kt
@@ -1,20 +1,28 @@
 package com.github.enteraname74.mouthpieces.app.feature.mainpage.composable
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import com.github.enteraname74.mouthpieces.domain.model.Mouthpiece
 
 @Composable
 fun MainPageResultList(
     mouthpieces: List<Mouthpiece>
 ) {
-    LazyColumn {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 250.dp),
+        contentPadding = PaddingValues(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
         items(
             items = mouthpieces
-        ) {
-            Text(it.name)
+        ) { mouthpiece ->
+            MainPageResultItem(mouthpiece)
         }
     }
 }
