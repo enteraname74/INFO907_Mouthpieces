3dba6ce70e400eae027e59528c0bf779b9b8fb88
enteraname74 <noahpenin@gmail.com>
2025-01-16 15:54:40 +0100

    Update json data

--- local/src/commonMain/kotlin/com/github/enteraname74/mouthpieces/local/data/JsonData.kt
+++ local/src/commonMain/kotlin/com/github/enteraname74/mouthpieces/local/data/JsonData.kt
@@ -14,28 +14,47 @@ val JsonData = """
                         "small": {
                           "Saxophone": {
                             "soprano": [
+                              "Selmer Concept",
+                              "Selmer S80 C*",
+                              "Selmer S90 170",
+                              "Yamaha 3C",
+                              "Yamaha 4C",
                               "Vandoren V5 S25",
                               "Vandoren V5 S15"
                             ],
                             "alto": [
+                              "Selmer Concept",
+                              "Selmer Claude Delangle",
+                              "Yamaha 3C",
+                              "Yamaha 4C",
                               "Vandoren V5 A27",
                               "Vandoren V5 A28"
+                            ],
+                            "tenor": [
+                              "Yamaha 3C"
                             ]
                           }
                         },
                         "medium": {
                           "Saxophone": {
                             "soprano": [
+                              "Selmer Super Session H",
                               "Vandoren V5 S35"
                             ],
                             "alto": [
+                              "Selmer S80 D",
+                              "Selmer S80 E",
+                              "Yamaha 5C",
+                              "Yamaha 6C",
                               "Vandoren V5 A15",
                               "Vandoren V5 A25",
                               "Vandoren V5 A20"
                             ],
                             "tenor": [
                               "Vandoren V5 T15",
-                              "Vandoren V5 T27"
+                              "Vandoren V5 T27",
+                              "Yamaha 4C",
+                              "Yamaha 5C"
                             ],
                             "baritone": [
                               "Vandoren V5 B25"
@@ -44,11 +63,23 @@ val JsonData = """
                         },
                         "large": {
                           "Saxophone": {
+                            "alto": [
+                              "Yamaha 7C"
+                            ],
                             "tenor": [
+                              "Selmer Concept",
+                              "Selmer S90 C*",
+                              "Selmer Soloist C*",
+                              "Yamaha 6C",
+                              "Yamaha 7C",
                               "Vandoren V5 T25",
                               "vandoren V5 T20"
                             ],
                             "baritone": [
+                              "Yamaha 5C",
+                              "Selmer S80 C*",
+                              "Selmer S80 C**",
+                              "Selmer S80 D",
                               "Vandoren V5 B27",
                               "Vandoren V5 B35"
                             ]
@@ -88,7 +119,9 @@ val JsonData = """
                               "Vandoren V16 S8"
                             ],
                             "alto": [
-                              "Vandoren V16 A5"
+                              "Selmer Jazz Flow",
+                              "Vandoren V16 A5",
+                              "Jody Jazz Jet 5"
                             ]
                           }
                         },
@@ -140,8 +173,21 @@ val JsonData = """
                   "Chamber": {
                     "round": {
                       "Opening": {
+                        "medium": {
+                          "Saxophone": {
+                            "alto": [
+                              "Otto Link Super Tonemaster Alto 5*",
+                              "Theo Wanne Earth II Alto 6"
+                            ]
+                          }
+                        },
                         "large": {
                           "Saxophone": {
+                            "alto": [
+                              "Theo Wanne Earth II Alto 7",
+                              "Jody Jazz DV 6*",
+                              "Otto Link Super Tonemaster Alto 8"
+                            ],
                             "tenor": [
                               "Vandoren V16 T5 Metal",
                               "Vandoren V16 T6 Metal"
@@ -180,6 +226,35 @@ val JsonData = """
                             "tenor": [
                               "Vandoren Jumbo Java T45",
                               "Vandoren Jumbo Java T50"
+                            ]
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            "metal": {
+              "Baffle": {
+                "step": {
+                  "Chamber": {
+                    "round": {
+                      "Opening": {
+                        "medium": {
+                          "Saxophone": {
+                            "alto": [
+                              "Jody Jazz Jet 5",
+                              "Jody Jazz Super Jet 5"
+                            ]
+                          }
+                        },
+                        "large": {
+                          "Saxophone": {
+                            "alto": [
+                              "Jody Jazz Super Jet 7",
+                              "Theo Wanne Fire II Alto 6",
+                              "Theo Wanne Fire II Alto 7"
                             ]
                           }
                         }
